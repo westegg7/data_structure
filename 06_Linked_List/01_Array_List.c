3f8ebca058736b702d24acf71e1bb88264b08727
westegg7 <westegg710@gmail.com>
2023-03-22 07:56:53 +0900

    licked-list01

---
+++ 06_Linked_List/01_Array_List.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LIST_SIZE 100
+
+typedef int element;
+typedef struct {
+    element array[MAX_LIST_SIZE];
+    int size;
+} ArrayListType;
+
+void error(char* msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(1);
+}
+
+void init(ArrayListType *L) {
+    L->size = 0;
+}
+
+int is_empty(ArrayListType *L) {
+    return L->size == 0;
+}
+
+int is_full(ArrayListType *L) {
+    return L->size == MAX_LIST_SIZE;
+}
+
+element get_entry(ArrayListType *L, int pos) {
+    if (pos < 0 || pos >= L->size) {
+        error("index error");
+    }
+    return L->array[pos];
+}
+
+void print_list(ArrayListType *L) {
+    for (int i = 0; i < L->size; i++) {
+        printf("%d->", L->array[i]);
+    }
+    printf("\n");
+}
+
+void insert_last(ArrayListType *L, element item) {
+    if (L->size >= MAX_LIST_SIZE) {
+        error("list overflow");
+    }
+    L->array[L->size++] = item;
+}
+
+void insert(ArrayListType *L, int pos, int item) {
+    if (!is_full(L) && pos >= 0 && pos <= L->size) {
+        for (int i = L->size - 1; i >= pos; i--) {
+            L->array[i+1] = L->array[i];
+        }
+        L->array[pos] = item;
+        L->size++;
+    }
+}
+
+element delete(ArrayListType *L, int pos) {
+    element item;
+    if (pos < 0 || pos >= L->size) 
+        error("index error");
+    item = L->array[pos];
+    for (int i = pos; i < (L->size - 1); i++) 
+        L->array[i] = L->array[i+1];
+    L->size--;
+    return item;
+}
+
+int main(void)
+{
+    ArrayListType list;
+
+    init(&list);
+    insert(&list, 0, 10);
+    print_list(&list);
+    insert(&list, 0, 20);
+    print_list(&list);
+    insert(&list, 0, 30);
+    print_list(&list);
+    insert_last(&list, 40);
+    print_list(&list);
+    delete(&list, 0);
+    print_list(&list);
+
+    return 0;
+}
